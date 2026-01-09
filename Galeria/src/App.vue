df587d19c5d9b7eb2ffa71e881de035eb0e5e3dc
GuilhermeKaua257 <guilhermeoliver257@gmail.com>
2026-01-09 21:52:54 +0000

    push

---
+++ Galeria/src/App.vue
@@ -0,0 +1,95 @@
+<template>
+  <div  class="header">
+    <h1>Galeria de Fotos</h1>
+    <div class="search">
+      <span class="search"><i class="bi bi-search"></i></span>
+    <input type="text" size="80%" class="form-control" placeholder="Busque Suas Fotos...">
+    <i class="bi bi-images"></i>
+     
+    </div>
+    
+  </div>
+<hr>
+ <section>
+     <div class="Galeria">
+         <div class="img-galeria h2 w-2 h-2">
+            <img src="https://images.unsplash.com/photo-1507525428034-b723cf961d3e?q=80&w=873&auto=forma
+            t&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+            <h2>Praia</h2>
+            
+     </div>
+          <div class="img-galeria w-2">
+            <img src="https://images.unsplash.com/photo-1543109687-93d0dc3a3a32?q=80&w=870&auto=format&f
+            it=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+            <h2>Aguia</h2>
+     </div>
+          <div class="img-galeria">
+             <img src="https://images.unsplash.com/photo-1511512578047-dfb367046420?q=80&w=871&auto=format
+             &fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+             <h2>Jogos</h2>
+     </div>
+          <div class="img-galeria h-2">
+             <img src="https://images.unsplash.com/photo-1573455494060-c5595004fb6c?q=80&w=840&auto=format
+             &fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+             <h2>Ruas</h2>
+     </div>
+          <div class="img-galeria ">
+             <img src="https://plus.unsplash.com/premium_photo-1661715304145-e686bb947289?q=80&w=870&auto=format&fi
+             t=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+             <h2>Chuva</h2>
+     </div>
+          <div class="img-galeria w-2">
+            <img src="https://images.unsplash.com/photo-1766329710745-21f110fa2d62?q=80&w=878&auto=format&fit=crop
+            &ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+            <h2>Espaço</h2>
+     </div>
+           <div class="img-galeria h-2 ">
+              <img src="https://images.unsplash.com/photo-1568772585432-0bcd9add44f5?q=80&w=387&auto=format&fit
+              =crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+              <h2>Moto</h2>
+     </div>
+          <div class="img-galeria ">
+              <img src="https://images.unsplash.com/photo-1513104890138-7c749659a591?q=80&w=870&auto=format
+              &fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+              <h2>Comida</h2>
+     </div>
+          <div class="img-galeria w-2">
+            <img src="https://images.unsplash.com/photo-1475070929565-c985b496cb9f?q=80&w=870&auto=format&fit
+            =crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+            <h2>Floresta</h2>
+     </div>
+          <div class="img-galeria w-2 h-2">
+             <img src="https://images.unsplash.com/photo-1712095312822-ef2c8c283099?q=80&w=871&auto=format&f
+             it=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+             <h2>Carro</h2>
+     </div>
+          <div class="img-galeria h-2">
+             <img src="https://images.unsplash.com/photo-1590420485404-f86d22b8abf8?q=80&w=1374&auto=format&f
+             it=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+             <h2>Lobo</h2>
+     </div>
+          <div class="img-galeria ">
+             <img src="https://images.unsplash.com/photo-1651012998667-2c779fee76f8?w=500&auto=format&fit=cr
+             op&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cGMlMjBnYW1lcnxlbnwwfHwwfHx8MA%3D%3D" alt="">
+             <h2>Computador</h2>
+
+     </div>
+   </div>
+  </section>
+  <hr>
+  <footer>
+  <div class="rodape">
+  <h2>© 2026 Galeria de Fotos.</h2>
+
+  <h2>
+    Todos os direitos
+reservados.
+  </h2>
+</div>
+
+
+
+  </footer>
+
+</template>
+
